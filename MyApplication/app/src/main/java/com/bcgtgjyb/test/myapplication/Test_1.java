7324c953f36dd4489a32e12c3bd5b590aaf06ab5
guohuanwen <1179636441@qq.com>
2015-08-21 19:09:07 +0800

    修复view闪烁bug

--- MyApplication/app/src/main/java/com/bcgtgjyb/test/myapplication/Test_1.java
+++ MyApplication/app/src/main/java/com/bcgtgjyb/test/myapplication/Test_1.java
@@ -5,10 +5,7 @@ import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AnimationSet;
 import android.widget.Button;
-
-import com.bcgtgjyb.test.mylibrary.AnimationCirculateListener;
 import com.bcgtgjyb.test.mylibrary.AnimationOnceListener;
 import com.bcgtgjyb.test.mylibrary.MovePath;
 import com.bcgtgjyb.test.mylibrary.MyAnimation;
@@ -28,20 +25,23 @@ public class Test_1 extends ActionBarActivity {
     private Button b3;
     private Button b4;
     private Button b5;
+    Handler handler;
+    Runnable runnable;
     List b3List,b2List,b4List,b5List,b3ListX,b2ListX,b4ListX,b5ListX,b3ListY,b2ListY,b4ListY,b5ListY;
     int open=-1;
     MovePath m=new MovePath();
     AnimatorSet animatorSet=new AnimatorSet();
     MyAnimation a=new MyAnimation();
-    List l2=m.getRandomDate(50, 1);
-    List l3=m.getRandomDate(50, 2);
-    List l4=m.getRandomDate(50, 3);
-    List l5=m.getRandomDate(50, 4);
+    List l2=m.getRandomDate(30, 1);
+    List l3=m.getRandomDate(30, 2);
+    List l4=m.getRandomDate(30, 3);
+    List l5=m.getRandomDate(30, 4);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_1);
         init();
+
     }
 
 
@@ -51,23 +51,15 @@ public class Test_1 extends ActionBarActivity {
         b4.clearAnimation();
         b5.clearAnimation();
         List list=new ArrayList();
-        list.add(a.setTranslation(b4,(List)l4.get(0),(List)l4.get(1),1000));
-        list.add(a.setTranslation(b2, (List) l2.get(0), (List) l2.get(1), 1000));
-        list.add(a.setTranslation(b3,(List)l3.get(0),(List)l3.get(1),1000));
-        list.add(a.setTranslation(b5,(List)l5.get(0),(List)l5.get(1),1000));
+        list.add(a.setTranslation(b4,(List)l4.get(0),(List)l4.get(1),3000));
+        list.add(a.setTranslation(b2, (List) l2.get(0), (List) l2.get(1), 3000));
+        list.add(a.setTranslation(b3,(List)l3.get(0),(List)l3.get(1),3000));
+        list.add(a.setTranslation(b5,(List)l5.get(0),(List)l5.get(1),3000));
         animatorSet=a.playTogether(list);
-        animatorSet.setDuration(5000);
+        animatorSet.setDuration(10000);
         animatorSet.addListener(new AnimationOnceListener());
 //        animatorSet.start();
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                b2.clearAnimation();
-                b3.clearAnimation();
-                b4.clearAnimation();
-                b5.clearAnimation();
-                setRondomMove();
-            }
-        }, animatorSet.getDuration());
+        handler.postDelayed(runnable, animatorSet.getDuration());
         animatorSet.start();
     }
 
@@ -78,6 +70,16 @@ public class Test_1 extends ActionBarActivity {
         b4=(Button)findViewById(R.id.button4);
         b5=(Button)findViewById(R.id.button5);
         b1.setOnClickListener(new MyButtonListener());
+        handler = new Handler();
+        runnable = new Runnable() {
+            public void run() {
+                b2.clearAnimation();
+                b3.clearAnimation();
+                b4.clearAnimation();
+                b5.clearAnimation();
+                setRondomMove();
+            }
+        };
         setRondomMove();
     }
 
@@ -95,10 +97,10 @@ public class Test_1 extends ActionBarActivity {
                 float x=b1.getX();
                 float[] coordinate=new float[]{x,y};
                 MovePath movePath=new MovePath();
-                b3List=movePath.getCurveData(getTranslation(b3, coordinate), 80, 1);
-                b2List=movePath.getCurveData(getTranslation(b2, coordinate), 80, 1);
-                b4List=movePath.getCurveData(getTranslation(b4, coordinate), 80, 1);
-                b5List=movePath.getCurveData(getTranslation(b5, coordinate), 80, 1);
+                b3List=movePath.getCurveData(getTranslation(b3, coordinate), 40, 1);
+                b2List=movePath.getCurveData(getTranslation(b2, coordinate), 40, 1);
+                b4List=movePath.getCurveData(getTranslation(b4, coordinate), 40, 1);
+                b5List=movePath.getCurveData(getTranslation(b5, coordinate), 40, 1);
 
                 b2ListX=(List)b2List.get(0);
                 b3ListX=(List)b3List.get(0);
@@ -134,18 +136,30 @@ public class Test_1 extends ActionBarActivity {
     }
 
     private void setClose(){
-
+        handler.removeCallbacks(runnable);
         List animationList=new ArrayList();
         MyAnimation myAnimation = new MyAnimation();
         animationList.add(myAnimation.setAlpha(b2,0,1000));
         animationList.add(myAnimation.setAlpha(b3, 0, 1000));
-        animationList.add(myAnimation.setTranslation(b2, b2ListX,b2ListY, 1000));
-        animationList.add(myAnimation.setTranslation(b3, b3ListX,b3ListY, 1000));
+        animationList.add(myAnimation.setTranslation(b2, b2ListX, b2ListY, 1000));
+        animationList.add(myAnimation.setTranslation(b3, b3ListX, b3ListY, 1000));
 
-        animationList.add(myAnimation.setAlpha(b4,0,1000));
+        animationList.add(myAnimation.setAlpha(b4, 0, 1000));
         animationList.add(myAnimation.setAlpha(b5, 0, 1000));
-        animationList.add(myAnimation.setTranslation(b4, b4ListX,b4ListY, 1000));
-        animationList.add(myAnimation.setTranslation(b5, b5ListX,b5ListY, 1000));
+        animationList.add(myAnimation.setTranslation(b4, b4ListX, b4ListY, 1000));
+        animationList.add(myAnimation.setTranslation(b5, b5ListX, b5ListY, 1000));
+
+        animationList.add(myAnimation.setScaleX(b2, 0.2f, 1000));
+        animationList.add(myAnimation.setScaleY(b2, 0.2f, 1000));
+
+        animationList.add(myAnimation.setScaleX(b3, 0.2f, 1000));
+        animationList.add(myAnimation.setScaleY(b3, 0.2f, 1000));
+
+        animationList.add(myAnimation.setScaleX(b4, 0.2f, 1000));
+        animationList.add(myAnimation.setScaleY(b4, 0.2f, 1000));
+
+        animationList.add(myAnimation.setScaleX(b5, 0.2f, 1000));
+        animationList.add(myAnimation.setScaleY(b5, 0.2f, 1000));
         myAnimation.playTogether(animationList).start();
         open=0;
     }
@@ -161,7 +175,7 @@ public class Test_1 extends ActionBarActivity {
         Collections.reverse(b5ListY);
         List animationList = new ArrayList();
         MyAnimation myAnimation = new MyAnimation();
-        animationList.add(myAnimation.setAlpha(b2,1,1000));
+        animationList.add(myAnimation.setAlpha(b2,1, 1000));
         animationList.add(myAnimation.setAlpha(b3, 1, 1000));
         animationList.add(myAnimation.setTranslation(b2, b2ListX, b2ListY, 1000));
         animationList.add(myAnimation.setTranslation(b3, b3ListX, b3ListY, 1000));
@@ -169,6 +183,14 @@ public class Test_1 extends ActionBarActivity {
         animationList.add(myAnimation.setAlpha(b5, 1, 1000));
         animationList.add(myAnimation.setTranslation(b4, b4ListX, b4ListY, 1000));
         animationList.add(myAnimation.setTranslation(b5, b5ListX, b5ListY, 1000));
+        animationList.add(myAnimation.setScaleX(b2, 1f, 1000));
+        animationList.add(myAnimation.setScaleY(b2, 1f, 1000));
+        animationList.add(myAnimation.setScaleX(b3, 1f, 1000));
+        animationList.add(myAnimation.setScaleY(b3,1f,1000));
+        animationList.add(myAnimation.setScaleX(b4, 1f, 1000));
+        animationList.add(myAnimation.setScaleY(b4,1f,1000));
+        animationList.add(myAnimation.setScaleX(b5, 1f, 1000));
+        animationList.add(myAnimation.setScaleY(b5,1f,1000));
         AnimatorSet animatorSet=myAnimation.playTogether(animationList);
         animatorSet.setDuration(1000);
 
@@ -180,7 +202,7 @@ public class Test_1 extends ActionBarActivity {
                 b3.clearAnimation();
                 b4.clearAnimation();
                 b5.clearAnimation();
-//                setRondomMove();
+                setRondomMove();
             }
         }, animatorSet.getDuration());
         animatorSet.start();
